64a497f7e7d6e617c2a2c44467bac3bfd3c8dce0
slakat <slakat@gmail.com>
2014-06-16 02:09:28 -0400

    categorias

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140616020346) do
+
+  create_table "products", force: true do |t|
+    t.integer  "type"
+    t.integer  "model"
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
