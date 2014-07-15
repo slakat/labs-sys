f26ed8c9053e4b479a95357f401a13759c229e36
slakat <slakat@gmail.com>
2014-07-15 19:25:38 -0400

    clients

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,39 @@
 // Loads all Bootstrap javascripts
 
 //= require bootstrap
+
+
+function goclients(){
+
+	var $clientcarousel = $('#clients-list');
+	var clients = $clientcarousel.children().length;
+	var clientwidth = (clients * 140); // 140px width for each client item 
+	$clientcarousel.css('width',clientwidth);
+	alert(clients);
+	 
+	var rotating = true;
+	var clientspeed = 1800;
+	var seeclients = setInterval(rotateClients, clientspeed);
+
+};
+
+
+ $(document).on({
+    mouseenter: function(){
+      rotating = false; // turn off rotation when hovering
+      alert("in")
+    },
+    mouseleave: function(){
+      rotating = true;
+    }
+  }, '#clients');
+   
+  function rotateClients() {
+    if(rotating != false) {
+      var $first = $('#clients-list li:first');
+      $first.animate({ 'margin-left': '-140px' }, 600, function() {
+        $first.remove().css({ 'margin-left': '0px' });
+        $('#clients-list li:last').after($first);
+      });
+    }
+  };
