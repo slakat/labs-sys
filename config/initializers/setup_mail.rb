a0a747dff5505a9f55abd6b70baada464ee7a0db
Pietro R <pietrord@hotmail.com>
2014-07-28 11:28:43 -0400

    Autentification correct

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",  
   :port                 => 587,  
   :domain               => "gmail.com",  
-  :user_name            => "easycookuc",  
-  :password             => "cookingiseasy",  
-  :authentication       => "plain",  
+  :user_name            => "sysdeingsender",  
+  :password             => "marin0319",  
+  :authentication       => :login,  
   :enable_starttls_auto => true  
 }
