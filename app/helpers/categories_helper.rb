cc6025f6f25f518422db33587977ccfa4d1fc665
slakat <slakat@gmail.com>
2014-07-28 22:55:26 -0400

    helper for cats

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,12 @@
 module CategoriesHelper
+
+	def get_url(img,cat,title)
+		@type= Category.where(:model=>cat)
+		if @type.size==1
+			link_to image_tag(img),category_url(@type.first.id)
+		else
+			link_to image_tag(img), :controller => "categories", :action => "index_category", :cat => cat, :title=>title , remote:true
+		end
+	end
+
 end
